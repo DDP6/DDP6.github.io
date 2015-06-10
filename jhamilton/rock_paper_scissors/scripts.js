2f6fa069691048ff308cc334a2e8f20e953ebfd7
Derpsteel <epicmaster95@gmail.com>
2015-06-10 10:39:03 -0700

    SO BETTER MANY FIX WOW
    I made everything better. Everything.

--- jhamilton/rock_paper_scissors/scripts.js
+++ jhamilton/rock_paper_scissors/scripts.js
@@ -84,7 +84,7 @@ function outCome() {
 function win() {
 	document.getElementById("resultPic").src = "http://img1.wikia.nocookie.net/__cb20061216202527/wikiality/images/3/31/Happy_kity.jpg"
 
-	document.getElementById("result").innerHTML ='<p id="border">You Won! Good job!.</p>'
+	document.getElementById("result").innerHTML ='<p id="border" class="text-center">You Won! Good job!.</p>'
 
 }
 
@@ -92,15 +92,15 @@ function win() {
 function lose() {
 	document.getElementById("resultPic").src = "http://fc09.deviantart.net/fs71/f/2014/025/8/b/sad_kitten_by_andyhulk-d73p1i0.jpg"
 
-	document.getElementById("result").innerHTML ='<p id="border">You Lost. :C</p>'
+	document.getElementById("result").innerHTML ='<p id="border" class="text-center">You Lost. :C</p>'
 }
 
 
 function tie() {
 	document.getElementById("resultPic").src = "http://www.thatcutesite.com/uploads/2010/06/cat_with_tie_02-600x450.jpg"
 
-	document.getElementById("result").innerHTML ='<p id="border">You tied.</p>'
+	document.getElementById("result").innerHTML ='<p id="border" class="text-center">You tied.</p>'
 }
 
-// This function makes the result text appear with a border that isn't there until this function happens
+
 
